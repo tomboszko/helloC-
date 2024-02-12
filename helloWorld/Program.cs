0c9ad7e10461bc6b52eff5f29e26c6a95b54b2f2
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-02-12 18:04:21 +0100

    Update helloWorld/Program.cs to prompt user for name and response

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, who are you?");
+string? name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}! How are you?");
+string? response = Console.ReadLine();
+
+if (response?.ToLower() == "good")
+{
+    Console.WriteLine($"I feel Good as well, {name} :)");
+}
+else
+{
+    Console.WriteLine($"I hope your day gets better, {name}.");
+}

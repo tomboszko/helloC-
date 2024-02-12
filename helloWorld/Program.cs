dda52589417cfc68bd23fd1354f5cf306908a3ae
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-02-12 18:44:32 +0100

    Add Hangman game to the program

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -68,3 +68,65 @@ else
 {
     Console.WriteLine("Okay, maybe next time!");
 }
+
+// pendu
+Console.WriteLine(" You're good at math, are yuou so good with word ? Would you like to play Hangman now? (yes/no)");
+string? playHangman = Console.ReadLine();
+if (playHangman?.ToLower() == "yes")
+{
+    List<string> wordList = new List<string> { "balls", "ass", "dick", "pussy", "boobs" };
+    Random random = new Random();
+    string wordToGuess = wordList[random.Next(wordList.Count)];
+    string wordToDisplay = new string('_', wordToGuess.Length);
+    int lives = 5; // nombre de vie(peut être essayer d'adapter au nombre de lettres du mot)
+    List<char> guessedLetters = new List<char>();
+
+    Console.WriteLine("Welcome to Hangman!");
+    while (lives > 0 && wordToDisplay.Contains('_'))
+    {
+        Console.WriteLine($"Word: {wordToDisplay} Lives left: {lives}");
+        Console.Write("Guess a letter: ");
+        char guess = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (guessedLetters.Contains(guess))
+        {
+            Console.WriteLine($"You've already guessed '{guess}'. Try a different letter.");
+            continue;
+        }
+
+        guessedLetters.Add(guess);
+
+        if (wordToGuess.Contains(guess))
+        {
+            Console.WriteLine($"Good guess! '{guess}' is in the word.");
+            for (int i = 0; i < wordToGuess.Length; i++)
+            {
+                if (wordToGuess[i] == guess)
+                {
+                    wordToDisplay = wordToDisplay.Remove(i, 1).Insert(i, guess.ToString());
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, '{guess}' is not in the word. Try again.");
+            lives--;
+        }
+    }
+
+    if (lives > 0)
+    {
+        Console.WriteLine($"Congratulations, {name}! You've won! The word was '{wordToGuess}'.");
+    }
+    else
+    {
+        Console.WriteLine($"Out of lives! The word was '{wordToGuess}'. Better luck next time, {name}.");
+    }
+}
+else
+{
+    Console.WriteLine("Okay, maybe some other time!");
+}
+
+Console.WriteLine("Thank you for playing. Goodbye!");
--- helloWorld/obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
+++ helloWorld/obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c9ad7e10461bc6b52eff5f29e26c6a95b54b2f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b1966fe9bbabb8689161d33eaa4e4018bd1410")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

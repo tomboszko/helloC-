e6742fc29a2e87e373a95328a84843807ea8e47b
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-02-13 08:54:46 +0100

    Update assembly version in helloWorld.AssemblyInfo.cs

--- helloWorld/obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
+++ helloWorld/obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b1966fe9bbabb8689161d33eaa4e4018bd1410")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda52589417cfc68bd23fd1354f5cf306908a3ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
